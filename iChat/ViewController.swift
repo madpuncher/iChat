f00450bef183d63b932e803aff9679796b03b58c
Eldar Tengizov <75984018+madpuncher@users.noreply.github.com>
2021-08-08 12:33:46 +0300

    Initial Commit

---
+++ iChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iChat
+//
+//  Created by Eʟᴅᴀʀ Tᴇɴɢɪᴢᴏᴠ on 08.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
